bc5c4cef990a7e8f678702f5d35ee14e65f4f0dd
Mijux <mijux-dev@proton.me>
2024-03-20 19:31:46 +0100

    fully-multilanguage + toml to yaml for configuration

---
+++ static/js/language-switcher.js
@@ -0,0 +1,21 @@
+function changeLanguage() {
+    selector = document.getElementById("select-language");
+    location.href = selector.value
+}
+
+function showLanguageDropdown() {
+  document.getElementById("language-dropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
